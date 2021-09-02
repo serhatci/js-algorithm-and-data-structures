d0f8dd2cd3233099027aaa7fa7c56950fa61cfcf
serhat <36408498+serhatci@users.noreply.github.com>
2021-09-02 09:10:50 +0200

    Fix binary search algorithm

--- src/searching-algorithms/binary-search.js
+++ src/searching-algorithms/binary-search.js
@@ -1,20 +1,20 @@
 // divide & and conquer - only for sorted data
 
 function binarySearch(arr, val) {
-  let middleIndex = Math.floor(arr.length - 1 / 2);
   let max = arr.length - 1;
   let min = 0;
+  let middleIndex = Math.floor((max + min) / 2);
 
   while (val !== arr[middleIndex]) {
-    if (val > middleIndex) {
-      min = middleIndex;
+    if (val > arr[middleIndex]) {
+      min = middleIndex + 1;
     } else {
-      max = middleIndex;
+      max = middleIndex - 1;
     }
-    middleIndex = Math.floor((max - min) / 2 + min);
+    middleIndex = Math.floor((max + min) / 2);
   }
 
   return middleIndex;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
