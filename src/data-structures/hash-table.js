7bf33bae892cfdb2ab824076033d4de463a11188
serhat <36408498+serhatci@users.noreply.github.com>
2021-10-21 02:52:53 +0200

    initialize hash-table.js

---
+++ src/data-structures/hash-table.js
@@ -0,0 +1,82 @@
+class HashTable {
+  constructor() {
+    this.size = 0;
+    this.table = new Array(3);
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        // Find the key/value pair in the chain
+        if (this.table[index][i][0] === key) {
+          this.table[index][i][1] = value;
+          return;
+        }
+      }
+      // not found, push a new key/value pair
+      this.table[index].push([key, value]);
+    } else {
+      this.table[index] = [];
+      this.table[index].push([key, value]);
+    }
+    this.size++;
+  }
+
+  get(key) {
+    const target = this._hash(key);
+    if (this.table[target]) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[target][i][0] === key) {
+          return this.table[target][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % this.table.length;
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+
+    if (this.table[index] && this.table[index].length) {
+      for (let i = 0; i < this.table.length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index].splice(i, 1);
+          this.size--;
+          return true;
+        }
+      }
+    } else {
+      return false;
+    }
+  }
+
+  display() {
+    this.table.forEach((values, index) => {
+      const chainedValues = values.map(
+        ([key, value]) => `[ ${key}: ${value} ]`
+      );
+      console.log(`${index}: ${chainedValues}`);
+    });
+  }
+}
+
+// TEST
+const ht = new HashTable();
+
+ht.set("serhat", 40);
+ht.set("ali", 41);
+
+ht.display();
+
+console.log(ht.get("serhat"));
+ht.remove("serhat");
+ht.display();
